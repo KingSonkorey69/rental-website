16b3cf47c6ffc372022ce3dded6bf3c60206a2a2
King Sonkorey <sam.kimotho450@gmail.com>
2022-05-17 20:47:28 +0300

    Changes made.
    1. Used the css flexbox gap to space the cards and also reduced the font of the navbars.

--- src/app/home.service.ts
+++ src/app/home.service.ts
@@ -13,7 +13,9 @@ export class HomeService {
       {img:'../assets/images/home2.jpg', name:'House 2',  price:'$200'},
       {img:'../assets/images/home3.jpg', name:'House 3',  price:'$200'},
       {img:'../assets/images/home4.jpg', name:'House 4',  price:'$205'},
-      {img:'../assets/images/home5.jpg', name:'House 5',  price:'$205'}
+      {img:'../assets/images/home5.jpg', name:'House 5',  price:'$205'},
+      {img:'../assets/images/home6.jpg', name:'House 6',  price:'$205'},
+      {img:'../assets/images/home7.jpg', name:'House 7',  price:'$205'}
     ]
   }
 }
